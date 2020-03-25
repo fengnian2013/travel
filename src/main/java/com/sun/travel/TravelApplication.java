4740e2224786061d0d0387a7400ba273c420fe82
sunqinghe@cheyipai.com <a1b2c3d4>
2020-03-25 10:09:09 +0800

    解决跨域带来的swagger失效问题,放弃过滤器的形式,使用WebMvcConfigurationSupport

--- src/main/java/com/sun/travel/TravelApplication.java
+++ src/main/java/com/sun/travel/TravelApplication.java
@@ -3,15 +3,21 @@ package com.sun.travel;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.sun.travel.mapper")
 @EnableSwagger2
-public class TravelApplication {
+public class TravelApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TravelApplication.class);
+    }
 }
---
+++ src/main/java/com/sun/travel/filter/CorsFilter.java
@@ -0,0 +1,43 @@
+package com.sun.travel.filter;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.LogRecord;
+
+/**
+ * @class: CorsFilter <br>
+ * @author: sun <br>
+ * @date: 2020-03-23 20:51
+ */
+@Configuration
+public class CorsFilter extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+        super.addCorsMappings(registry);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
+}
